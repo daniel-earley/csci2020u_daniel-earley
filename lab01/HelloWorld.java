00fadde9033529a4025df9e0fb739f82c415dcd6
daniel-earley <danielse08@gmail.com>
2021-01-20 12:15:41 -0500

    pain

--- lab01/HelloWorld.java
+++ lab01/HelloWorld.java
@@ -1,11 +1,6 @@
 class HelloWorld{
     public static void main(String[] args){
-<<<<<<< HEAD
-
-=======
         // I accidentally did the steps out of order last time
         System.out.println("Making another change");
->>>>>>> b
-	>>>>>>>
     }
 }
